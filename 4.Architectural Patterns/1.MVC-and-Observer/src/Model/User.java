1a8597d85c6107f6c24c651de287c93a47cc56d2
Krasimir Stoyanov <developerstoyanov5@gmail.com>
2017-01-26 20:59:29 +0200

    Created Bean object for the User.

---
+++ 4.Architectural Patterns/1.MVC-and-Observer/src/Model/User.java
@@ -0,0 +1,40 @@
+package Model;
+
+/**
+ * Created by Krasimir Stoyanov on 26-Jan-17.
+ */
+
+// Bean class for User
+public class User {
+    private int id;
+    private String name;
+    private String password;
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
